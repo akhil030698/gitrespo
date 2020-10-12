fa8da1ed6c18a357a51f18f0f23bea585fa6b7be
Akhilesh <Akhilesh@LAPTOP-098RHR0O.domain.name>
2020-10-12 18:02:23 +0530

    commit

---
+++ akhilesh/src/akhilesh/Add.java
@@ -0,0 +1,16 @@
+package akhilesh;
+
+import java.util.Scanner;
+
+public class Add {
+	public static void main(String[] args) {
+		  System.out.println("Enter no.");
+    	  Scanner n = new Scanner(System.in);
+    	  int i = n.nextInt();
+    	  if (i%2 == 0){
+    		  System.out.println("Even");
+    	  }
+    	  else
+    		  System.out.println("Odd");
+	}
+}
