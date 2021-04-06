4245df09c28c0fd721b2f28195f557bcff922977
Akhilesh <Akhilesh@LAPTOP-098RHR0O>
2021-04-06 15:31:07 +0530

    commit

--- akhilesh/src/akhilesh/hellogit.java
+++ akhilesh/src/akhilesh/hellogit.java
@@ -4,6 +4,6 @@ public class hellogit {
 	public static void main(String[] args) {
 		System.out.println(" hello ");
 		System.out.println(" I write in github"); 
-		System.out.println(" I write in eclipse");
+	
 	}
 }
