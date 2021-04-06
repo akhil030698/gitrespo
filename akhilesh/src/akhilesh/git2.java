3ff069565ebd7ddd61b054649ae8b25e803ef973
Akhilesh <Akhilesh@LAPTOP-098RHR0O>
2021-04-06 15:28:27 +0530

    commit once again

---
+++ akhilesh/src/akhilesh/git2.java
@@ -0,0 +1,20 @@
+package akhilesh;
+
+import java.util.stream.IntStream;
+
+public class git2 {
+	public static void main(String[] args) {
+		System.out.println("What will be o/p");
+		IntStream.rangeClosed(5, 10)// int stream 5 10
+				.filter(i -> {
+					System.out.println("Filtering " + i);
+					return i % 2 == 0;
+				})// in stream of all even ints bet 5 10
+				.map(i -> {
+					System.out.println("Mapping " + i);
+					return i + 1;
+				});
+		
+
+	}
+}
